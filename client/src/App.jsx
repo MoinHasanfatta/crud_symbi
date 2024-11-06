7228327ec8adeb896cfbb5a99aeafcbbc77e6d02
Moin Hasanfatta <hasanfattamoin@gmail.com>
2024-11-06 08:50:58 +0530

    My Crud App

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+// import { useState } from 'react' 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import User from './User'
+import CreateUser from './CreateUser'
+import UpdateUser from './UpdateUser'
+
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div>
+     <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<User />}></Route>
+        <Route path='/create' element={<CreateUser />}></Route>
+        <Route path='/update/:id' element={<UpdateUser />}></Route>
+      </Routes>
+     </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
